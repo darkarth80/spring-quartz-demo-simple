efb218fe971133de9fccd509873b1e6171f7f5c2
Arturo Cuevas <26891629+darkarth80@users.noreply.github.com>
2019-08-20 01:31:53 -0500

    Se agrega codigo base.

---
+++ src/main/java/com/darkarth/spring/boot/quartz/demo/SpringQuartzDemoSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.darkarth.spring.boot.quartz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuartzDemoSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuartzDemoSimpleApplication.class, args);
+	}
+
+}
